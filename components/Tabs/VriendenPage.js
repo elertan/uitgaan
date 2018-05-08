cbffe823493aa051343ab84740558acf63612712
elertan <denkievits@gmail.com>
2018-05-08 16:23:10 +0200

    Vrienden toegevoegd (nog steeds vriendloos)

--- components/Tabs/VriendenPage.js
+++ components/Tabs/VriendenPage.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import {
-    Text,
-    View
-} from 'native-base';
+import { Image } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right, View } from 'native-base';
+
+const friends = [
+    { name: 'Gavin den Hollander' },
+    { name: 'Dennis Kievits' },
+    { name: 'Bart Simons' },
+    { name: 'Kelvin' },
+    { name: 'Jurian Vink' },
+    { name: 'Rechterhand' },
+    { name: 'Google' },
+];
 
 export default class VriendenPage extends React.Component {
     render() {
         return (
-            <View>
-            <Text>Je hebt geen vrienden</Text>
-            </View>
+            <Container>
+                <Content>
+                    {friends.map((friend, i) => {
+                        return (
+                            <Card key={i}>
+                                <CardItem>
+                                <Left>
+                                    <View style={{
+                                        height: 35,
+                                        width: 35,
+                                        backgroundColor: '#121212',
+                                        borderRadius: 50,
+                                        marginRight: 10
+                                    }} />
+                                    {/* <Image source={} /> */}
+                                    <Left>
+                                    <Text>{friend.name}</Text>
+                                    </Left>
+                                </Left>
+                                </CardItem>
+                                {/* <CardItem cardBody>
+                                <Image source={{uri: locatie.imageUrl }} style={{height: 200, width: null, flex: 1}}/>
+                                </CardItem> */}
+                            </Card>
+                        );
+                    })}
+                </Content>
+            </Container>
         );
     }
 }
