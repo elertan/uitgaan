60b3b137fa4939c28616a5f3bf1160644e268f30
elertan <denkievits@gmail.com>
2018-05-08 10:30:16 +0200

    Font fix

--- App.js
+++ App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import Root from './components/Root';
 import DrawerContent from './components/DrawerContent';
 import { Drawer } from 'native-base';
+import Expo from 'expo';
 
 export default class App extends React.Component {
+    async componentDidMount() {
+        await Expo.Font.loadAsync({
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+        });
+    }
+
     closeDrawer = () => {
         this.drawer._root.close();
     }
